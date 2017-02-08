ece025ac0e8e67532c45bac3ce92ac7a2d7068d0
BSpirit <clonier.tony@hotmail.fr>
2017-02-08 10:18:53 +0100

    Ajout de commentaires

--- 2048.c
+++ 2048.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef struct{
-	int n; //taille de la grille (n*n)
+	int n; //taille de la grille = n*n 
 	int valMax; // valeur à atteindre pour gagner
 	int nbCasesLibres; //nbre de cases libres sur la grille
 	int *grille;
@@ -10,26 +10,28 @@ typedef struct{
 
 /*! * void initialiseJeu(jeu *p, int n, int valMax)
 	*
+	* Initialise les champs n et valmax avec les valeurs passées en paramètre, initialise nbCasesLibres
 	* Alloue la grille de la variable jeu passée par adresse
 	* Initialise les cases de la grille avec des cases vides  (valeurs nulles)
-	* Initialise les champs n et valmax avec les valeurs passées en paramètre
 	*
 	* \param p : pointeur sur une grille de 2018
 	* \param n : taille de la grille
 	* \param valMax : valeur à atteindre pour gagner
 	*/
 void initialiseJeu(jeu *p, int n, int valMax){
-
+	//Initialisation des variables
 	p->n = n;
-	p->nbCasesLibres = n*n; //La grille possède n*n cases
+	p->nbCasesLibres = n*n; 
 	p->valMax = valMax;
 
+	//allocation dynamique de la taille de la grille
 	p->grille = (int*) malloc(p->nbCasesLibres*sizeof(int));
 	if(p->grille==NULL){
 		printf("Erreur allocation ! \n");
 		exit(-1);
 	}
 
+	//Initialisation des cases de la grille à 0
 	int i;
 	for(i=0;i<p->nbCasesLibres;i++)
 		p->grille[i] = 0;
@@ -42,7 +44,6 @@ void initialiseJeu(jeu *p, int n, int valMax){
 	* \param p : pointeur sur une partie de 2048
 	*/
 void libereMemoire(jeu *p){
-
 	free(p->grille);
 }
 
@@ -55,8 +56,7 @@ void libereMemoire(jeu *p){
 	* \param j : entier correspondant au numéro de "colonne"
 	*/
 int indiceValide(jeu *p, int i, int j){
-
-	return(i>=0 && i < p->n && j>=0 && j < p->n);
+	return (i>=0 && i < p->n && j>=0 && j < p->n);
 }
 
 /*! * int getVal(jeu *p, int ligne, int colonne)
@@ -68,11 +68,10 @@ int indiceValide(jeu *p, int i, int j){
 	* \param colonne : entier correspondant au numéro de colonne
 	*/
 int getVal(jeu *p, int ligne, int colonne){
-
 	if(indiceValide(p, ligne, colonne))
 		return p->grille[p->n*ligne+colonne];
-	else
-		return -1;
+	
+	return -1;
 }
 
 /*! * int caseVide(jeu *p, int i, int j)
@@ -85,32 +84,31 @@ int getVal(jeu *p, int ligne, int colonne){
 	* \param j : entier correspondant au numéro de colonne
 	*/
 int caseVide(jeu *p, int i, int j){
-	if(indiceValide(p, i, j)){
-		if(getVal(p, i, j)==0)
-			return 1;
-	}
+	if(getVal(p, i, j)==0)
+		return 1;
 
 	return 0;
 }
 
-/*! * void setVal(jeu *p, int ligne, int colonne, int val)
+/*! * int setVal(jeu *p, int ligne, int colonne, int val)
  	*
  	* Fonction modifiant la valeur de la case (ligne, colonne) de la partie p
  	* avec la valeur val
+	* Retourne 1 si une valeur a été assignée à une case existante
+	* Retourne 0 sinon
  	*
  	* \param p : pointeur sur la partie en cours
 	* \param ligne : entier correspondant au numéro de ligne
 	* \param colonne : entier correspondant au numéro de colonne
 	* \param val : entier à mettre dans la case (i,j) (si elle existe)
 	*/
-void setVal(jeu *p, int ligne, int colonne, int val){
-
-
+int setVal(jeu *p, int ligne, int colonne, int val){
 	if(indiceValide(p, ligne, colonne)){
-		if(caseVide(p, ligne, colonne))
-			p->nbCasesLibres-=1;
 		p->grille[p->n*ligne+colonne] = val;
+		return 1;
 	}
+
+	return 0;
 }
 
 /*! * void affichage(jeu * p)
@@ -120,7 +118,6 @@ void setVal(jeu *p, int ligne, int colonne, int val){
 	*
 	*/
 void affichage(jeu * p){
-
 	int i;
 	for(i=0;i<p->n;i++){
 		printf("\n");
@@ -137,22 +134,21 @@ void affichage(jeu * p){
  	* \param p : pointeur sur la partie en cours
  	*/
 int gagne(jeu *p){
-
-	int test = 0;
+	int flag = 0;
 	int i = 0;
 	int j = 0;
 
-	while(i<p->n && test==0){
+	while(i<p->n && flag==0){
 		j = 0;
-		while(j<p->n && test==0){
+		while(j<p->n && flag==0){
 			if(getVal(p, i, j)==p->valMax)
 				test = 1;
 			j++;
 		}
 		i++;
-	}
+	}//sortie : grille entièrement parcourue et/ou une case de la grille a pour valeur valMax (flag==1)
 
-	return test;
+	return flag;
 }
 
 /*! * int perdu(jeu *p)
